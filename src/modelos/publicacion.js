c9ba2353602512ea8213862b3c2ad2a03b5cb006
Coca <cocacore7@gmail.com>
2020-10-30 18:54:40 -0600

    Actualizacion Fecha Modelo

--- src/modelos/publicacion.js
+++ src/modelos/publicacion.js
@@ -6,7 +6,7 @@ var Esquemapublicacion = Schema({
     idPublicacion: Number,
     mensaje: String,
     carnet: { type: Schema.ObjectId, ref: "usuario" },
-    fecha: Date,
+    fecha: String,
     cursoCatedratico: { type: Schema.ObjectId, ref: "cursoCatedratico" },
     codigoCurso: { type: Schema.ObjectId, ref: "curso" },
     noCatedratico: { type: Schema.ObjectId, ref: "catedratico" },
